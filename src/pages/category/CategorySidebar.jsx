36bd8f82bf894cf751b32a67e7ba716a67f00d22
[Cloud3] 박원정 <josephwjp8@gmail.com>
2024-06-19 15:57:51 +0900

    Feat: 카테고리 및 브랜드 화면 수정

---
+++ src/pages/category/CategorySidebar.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { List, ListItem, ListItemText, TextField } from '@mui/material';
+
+const CategorySidebar = () => {
+    const [categories, setCategories] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:8080/category/all')
+            .then((response) => response.json())
+            .then((data) => setCategories(data))
+            .catch((error) => console.error('Error fetching data:', error));
+    }, []);
+
+    const filteredCategories = categories.filter((category) =>
+        category.categoryName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <div>
+            <TextField
+                label="카테고리 검색"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <List>
+                <ListItem button component={Link} to={`/category`}>
+                    <ListItemText primary="전체 보기" />
+                </ListItem>
+                {filteredCategories.map((category) => (
+                    <ListItem button key={category.id} component={Link} to={`/category/${category.id}`}>
+                        <ListItemText primary={category.categoryName} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+};
+
+export default CategorySidebar;
