13ad2f0ee9eb2f2964c56532b28bdf965258eb4b
[Cloud3] 박원정 <josephwjp8@gmail.com>
2024-06-21 02:01:44 +0900

    Chore: 홈 화면 광고 배너 사진 수정

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
     const navigate = useNavigate();
 
     const banners = [
-        'https://img.autocamping.co.kr/event/2024/240613Dair_main_PC.jpg',
+        'https://www.ocamall.com/design/ocamall/2022_renew/img/PC1.jpg',
         'https://img.autocamping.co.kr/event/2023/20231019SHELTER.jpg',
         'https://img.autocamping.co.kr/event/2022/event_0721.jpg',
     ];
